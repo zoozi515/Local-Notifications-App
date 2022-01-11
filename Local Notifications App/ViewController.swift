748afc032e7976c619f65a5df4e19ef52269962a
Zahra Al-Jafar <memo.jks357@hotmail.com>
2022-01-11 13:28:23 -0800

    Initial Commit

---
+++ Local Notifications App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Local Notifications App
+//
+//  Created by administrator on 11/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
